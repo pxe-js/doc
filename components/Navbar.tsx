6e79fdd33130794eee83f77729e059f19bedca2e
Revenity <87530663+aquapi@users.noreply.github.com>
2022-07-17 21:34:41 +0700

    Adding navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "@chakra-ui/react";
+import styles from "../styles/Home.module.scss";
+
+function Navbar() {
+    return <Stack direction="row" className={styles.navbar}></Stack>;
+}
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,21 +4,25 @@ import { colorScheme, color, contentInCenter } from '../utils/constants';
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import arrow from "../assets/arrow.svg";
+import Navbar from '../components/Navbar';
 
 const Home: NextPage = () => {
-	return <Stack {...contentInCenter} className={styles.wrapper} direction="column">
-		<Stack {...contentInCenter} className={styles.texts}>
-			<Heading color={color}>PXE</Heading>
-			<Text fontWeight={200}>A web framework for Node.js</Text>
+	return <>
+		<Navbar />
+		<Stack {...contentInCenter} className={styles.wrapper} direction="column">
+			<Stack {...contentInCenter} className={styles.texts}>
+				<Heading color={color}>PXE</Heading>
+				<Text fontWeight={200}>A web framework for Node.js</Text>
+			</Stack>
+			<Flex className={styles.buttons}>
+				<Button colorScheme={colorScheme}>Get Started</Button>
+				<Button colorScheme={colorScheme} variant="ghost" className={styles.goToDocs}>
+					<Text>Go to Docs</Text>
+					<Image src={arrow} alt="arrow" />
+				</Button>
+			</Flex>
 		</Stack>
-		<Flex className={styles.buttons}>
-			<Button colorScheme={colorScheme}>Get Started</Button>
-			<Button colorScheme={colorScheme} variant="ghost" className={styles.goToDocs}>
-				<Text>Go to Docs</Text>
-				<Image src={arrow} alt="arrow" />
-			</Button>
-		</Flex>
-	</Stack>;
+	</>;
 };
 
 export const getStaticProps: GetStaticProps = () => ({
