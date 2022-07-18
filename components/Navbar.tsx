bf0359e959907c27c51511ecd321da313d9cf704
Revenity <87530663+aquapi@users.noreply.github.com>
2022-07-18 11:42:32 +0700

    Add navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,21 @@
-import { Stack } from "@chakra-ui/react";
-import styles from "../styles/Home.module.scss";
+import { Heading, Stack, Link, Text } from "@chakra-ui/react";
+import styles from "../styles/components/Navbar.module.scss";
+import { color } from "../utils/constants";
 
 function Navbar() {
-    return <Stack direction="row" className={styles.navbar}></Stack>;
+    return <Stack direction="row" className={styles.navbar}>
+        <Link href="/">
+            <Heading color={color} fontWeight={800} size="md">PXE</Heading>
+        </Link>
+        <Stack direction="row" className={styles.links}>
+            <Link href="/docs">
+                <Text>Docs</Text>
+            </Link>
+            <Link href="https://github.com/pxe-js" target="_blank">
+                <Text>Github</Text>
+            </Link>
+        </Stack>
+    </Stack>;
 }
 
 export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Heading, Stack, Text } from '@chakra-ui/react';
+import { Button, Flex, Heading, Link, Stack, Text } from '@chakra-ui/react';
 import type { GetStaticProps, NextPage } from 'next';
 import { colorScheme, color, contentInCenter } from '../utils/constants';
 import Image from "next/image";
@@ -11,15 +11,19 @@ const Home: NextPage = () => {
 		<Navbar />
 		<Stack {...contentInCenter} className={styles.wrapper} direction="column">
 			<Stack {...contentInCenter} className={styles.texts}>
-				<Heading color={color}>PXE</Heading>
+				<Heading color={color} fontWeight={800}>PXE</Heading>
 				<Text fontWeight={200}>A web framework for Node.js</Text>
 			</Stack>
 			<Flex className={styles.buttons}>
+				<Link href="/begin">
 				<Button colorScheme={colorScheme}>Get Started</Button>
-				<Button colorScheme={colorScheme} variant="ghost" className={styles.goToDocs}>
-					<Text>Go to Docs</Text>
-					<Image src={arrow} alt="arrow" />
-				</Button>
+				</Link>
+				<Link href="/docs">
+					<Button colorScheme={colorScheme} variant="ghost" className={styles.goToDocs}>
+						<Text>Go to Docs</Text>
+						<Image src={arrow} alt="arrow" />
+					</Button>
+				</Link>
 			</Flex>
 		</Stack>
 	</>;
