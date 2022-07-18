ab4b452f41815a3ea264c4ee7f8bfbd0aa48dd51
Revenity <87530663+aquapi@users.noreply.github.com>
2022-07-18 12:33:32 +0700

    Some fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,23 @@ import Navbar from '../components/Navbar';
 const Home: NextPage = () => {
 	return <>
 		<Navbar />
-		<Stack {...contentInCenter} className={styles.wrapper} direction="column">
+		<Stack {...contentInCenter} className={styles.wrapper}>
+			<Flex className={styles.background}></Flex>
 			<Stack {...contentInCenter} className={styles.texts}>
-				<Heading color={color} fontWeight={800}>PXE</Heading>
-				<Text fontWeight={200}>A web framework for Node.js</Text>
+				<Heading color={color} fontWeight={800} fontSize="50px">PXE</Heading>
+				<Text fontWeight={100} fontSize="20px">A web framework for Node.js</Text>
 			</Stack>
 			<Flex className={styles.buttons}>
 				<Link href="/begin">
-				<Button colorScheme={colorScheme}>Get Started</Button>
+					<Button colorScheme={colorScheme} size="lg">Get Started</Button>
 				</Link>
 				<Link href="/docs">
-					<Button colorScheme={colorScheme} variant="ghost" className={styles.goToDocs}>
+					<Button 
+						colorScheme={colorScheme} 
+						size="lg" 
+						variant="ghost" 
+						className={styles.goToDocs}
+					>
 						<Text>Go to Docs</Text>
 						<Image src={arrow} alt="arrow" />
 					</Button>
